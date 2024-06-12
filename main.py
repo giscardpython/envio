c25bc082690b75298f041498b99b5d3f5bd73651
Giscard Stephanou <giscard.silva@aluno.senai.br>
2024-06-11 21:37:50 -0300

    Repositrio atualizado por automao.

--- main.py
+++ main.py
@@ -9,22 +9,33 @@ import time
 # tempo que cada comando demora para executar
 pyautogui.PAUSE = 1
 
-# continua git pushas instruções
+# Ex: Repositorio remoto https://github.com/giscardpython/teste4.git
+repositorio_remoto = str(input('Informe o repositório remoto do GitHub para onde devem ser enviados os arquivos: '))
 
-valor = str(input('Informe o repositório remoto do GitHub para onde devem ser enviados elo usuário: '))
+pyautogui.hotkey('ctrl', 'k', 'ctrl', 'o', "'")
 
-pyautogui.hotkey('ctrl', 'k', "'")
-pyautogui.hotkey('ctrl', 'o', "'")
+# espera 5 segundos para dar tempo de fazer a criação
+time.sleep(15)
 
 pyautogui.hotkey('ctrl', 'shift', "'")
 
 pyautogui.write('git init .')
 pyautogui.press('enter')
+
 pyautogui.write('git add .')
 pyautogui.press('enter')
+
 pyautogui.write('git commit -m "Repositório atualizado por automação."')
 # espera 5 segundos para dar tempo de fazer o commit
 time.sleep(5)
 # continua as instruções
 pyautogui.press('enter')
+
+pyautogui.write('git branch -M main')
+pyautogui.press('enter')
+
+pyautogui.write('git remote add origin main', repositorio_remoto)
+pyautogui.press('enter')
+
 pyautogui.write('git push')
+pyautogui.press('enter')
